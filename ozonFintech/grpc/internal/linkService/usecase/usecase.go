310058acfad7383f639dd4b6ab0be5d3cb554af4
ampheee <babbling@student.21-school.ru>
2023-06-11 23:38:17 +0300

    update

--- ozonFintech/grpc/internal/linkService/usecase/usecase.go
+++ ozonFintech/grpc/internal/linkService/usecase/usecase.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"context"
-	"errors"
 	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"grpcService/config"
 	"grpcService/grpc_domain"
 	"grpcService/internal/linkService"
@@ -13,6 +15,7 @@ import (
 	"grpcService/pkg/postgresql"
 	"grpcService/pkg/redisDB"
 	"grpcService/pkg/utilities"
+	"net/http"
 )
 
 type LinkUseCase struct {
@@ -26,22 +29,32 @@ func (l LinkUseCase) Get(ctx context.Context, r *grpc_domain.GetLinkRequest) (*g
 	originalLink, err := l.Repo.SelectLink(ctx, r.AbbreviatedLink)
 	if err != nil {
 		l.Logger.Warn().Err(err).Msg("unable to get link by abbreviated.")
-		return &grpc_domain.GetLinkResponse{OrigLink: ""}, err
+		if err.Error() == "redis: nil" || err.Error() == "no rows in result set" {
+			return &grpc_domain.GetLinkResponse{},
+				status.Error(http.StatusNotFound, "Link not found")
+		} else {
+			return &grpc_domain.GetLinkResponse{},
+				status.Error(http.StatusInternalServerError, "InternalServiceError")
+		}
 	}
-	return &grpc_domain.GetLinkResponse{OrigLink: originalLink}, nil
+	return &grpc_domain.GetLinkResponse{OrigLink: originalLink},
+		grpc.SendHeader(ctx, metadata.New(map[string]string{"status": "200"}))
 }
 
 func (l LinkUseCase) Create(ctx context.Context, r *grpc_domain.CreateLinkRequest) (*grpc_domain.CreateLinkResponse, error) {
 	if r.OrigLink == "" {
-		return &grpc_domain.CreateLinkResponse{}, errors.New("EmptyLink")
+		return &grpc_domain.CreateLinkResponse{},
+			status.Error(http.StatusBadRequest, "EmptyLink")
 	}
 	abbreviatedLink := utilities.EncodeBase63(utilities.HashLink(r.OrigLink))
 	l.Logger.Info().Msg(abbreviatedLink)
 	if err := l.Repo.InsertLink(ctx, abbreviatedLink, r.OrigLink); err != nil {
-		return &grpc_domain.CreateLinkResponse{}, err
+		return &grpc_domain.CreateLinkResponse{},
+			status.Error(http.StatusInternalServerError, "InternalServiceError")
 	}
 	l.Logger.Info().Msg("link saved and abbreviated returned.")
-	return &grpc_domain.CreateLinkResponse{AbbreviatedLink: "rus.tam/" + abbreviatedLink}, nil
+	return &grpc_domain.CreateLinkResponse{AbbreviatedLink: "rus.tam/" + abbreviatedLink},
+		grpc.SendHeader(ctx, metadata.New(map[string]string{"status": "200"}))
 }
 
 func NewLinkService(ctx context.Context, c config.Config) (grpc_domain.LinkServiceServer, error) {
